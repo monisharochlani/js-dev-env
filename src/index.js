00104d5629ac4858198f358192f77274b7a0bfb7
Monisha Rochlani <rochlani.monisha@gmail.com>
2018-12-21 17:50:04 -0800

    Configured linting using ESLint

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import numeral from 'numeral';
 import './index.css'
 
 const value = numeral(1000).format('$0,0.00');
-console.log(`I can pay ${value} for this course`);
+console.log(`I can pay ${value} for this course`); // eslint-disable-line no-console
