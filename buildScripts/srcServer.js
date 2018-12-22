e87cdca43f7f23e180c92ef641381145d1e94039
Monisha Rochlani <rochlani.monisha@gmail.com>
2018-12-21 17:04:39 -0800

    Configured bundling with webpack

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,18 +1,29 @@
 import express from 'express';
 import path from 'path';
 import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
 
 const port = 3000;
 
 // Instantiate express
 const app = express();
 
+//Instantiate webpack
+const compiler = webpack(config);
+
+// Configure express to use webpack
+app.use(require('webpack-dev-middleware')(compiler, {
+	noInfo: true,
+	publicPath: config.output.publicPath
+}));
+
 //Configure Route
 app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
-// Configure listeining on port
+// Configure listening on port
 app.listen(port, function (err) {
 	if (err) {
 		console.log(err);
